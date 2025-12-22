95e9a642570e0063b8e06f18640fce3114db3e4f
spiros <spiridonzervos@gmail.com>
2025-12-23 00:32:04 +0200

    228. Add the Character Definition Primary Asset

---
+++ Source/Crash/Private/Chatacter/PA_CharacterDefinition.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PA_CharacterDefinition.h"
+
+#include "CCharacter.h"
+
+FPrimaryAssetId UPA_CharacterDefinition::GetPrimaryAssetId() const
+{
+	return FPrimaryAssetId(GetCharacterDefinitionAssetType(), GetFName());
+}
+
+FPrimaryAssetType UPA_CharacterDefinition::GetCharacterDefinitionAssetType()
+{
+	return FPrimaryAssetType("CharacterDefinition");
+}
+
+FString UPA_CharacterDefinition::GetCharacterDisplayName() const
+{
+	return CharacterName;
+}
+
+UTexture2D* UPA_CharacterDefinition::LoadIcon() const
+{
+	return CharacterIcon.LoadSynchronous();
+}
+
+TSubclassOf<ACCharacter> UPA_CharacterDefinition::LoadCharacterClass() const
+{
+	return CharacterClass.LoadSynchronous();
+}
+
+TSubclassOf<UAnimInstance> UPA_CharacterDefinition::LoadDisplayAnimBPClass() const
+{
+	return DisplayAnimBPClass.LoadSynchronous();
+}
+
+USkeletalMesh* UPA_CharacterDefinition::LoadDisplayMesh() const
+{
+	TSubclassOf<ACCharacter> CharacterClass = LoadCharacterClass();
+	check(CharacterClass);
+	
+	ACharacter* Character = Cast<ACharacter>(CharacterClass.GetDefaultObject());
+	if (!IsValid(Character))
+	{
+		return nullptr;
+	}
+	
+	return Character->GetMesh()->GetSkeletalMeshAsset();
+}
---
+++ Source/Crash/Private/Chatacter/PA_CharacterDefinition.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PA_CharacterDefinition.generated.h"
+
+class ACCharacter;
+/**
+ * 
+ */
+UCLASS()
+class CRASH_API UPA_CharacterDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+	
+	static FPrimaryAssetType GetCharacterDefinitionAssetType();
+	
+	FString GetCharacterDisplayName() const;
+	
+	UTexture2D* LoadIcon() const;
+	
+	TSubclassOf<ACCharacter> LoadCharacterClass() const;
+	
+	TSubclassOf<UAnimInstance> LoadDisplayAnimBPClass() const;
+	
+	USkeletalMesh* LoadDisplayMesh() const;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	FString CharacterName;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftObjectPtr<UTexture2D> CharacterIcon;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftClassPtr<ACCharacter> CharacterClass;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Character")
+	TSoftClassPtr<UAnimInstance> DisplayAnimBPClass;
+};
