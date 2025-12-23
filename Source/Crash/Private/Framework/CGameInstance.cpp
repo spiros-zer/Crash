bc5f5d90447816a8dd9c9f1e9f271c3b57aaec6a
spiros <spiridonzervos@gmail.com>
2025-12-24 00:10:14 +0200

    246. Implement the Map Loading in the C Game Instance Class

---
+++ Source/Crash/Private/Framework/CGameInstance.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CGameInstance.h"
+
+void UCGameInstance::StartMatch()
+{
+	if (GetWorld()->GetNetMode() == NM_DedicatedServer || GetWorld()->GetNetMode() == NM_ListenServer)
+	{
+		LoadLevelAndListen(GameLevel);
+	}
+}
+
+void UCGameInstance::LoadLevelAndListen(TSoftObjectPtr<UWorld> Level)
+{
+	const FName LevelURL = FName(*FPackageName::ObjectPathToPackageName(Level.ToString()));
+	
+	if (LevelURL == "") return;
+	
+	GetWorld()->ServerTravel(LevelURL. ToString() + "?listen");
+}
---
+++ Source/Crash/Private/Framework/CGameInstance.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRASH_API UCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	
+	void StartMatch();
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> MainMenuLevel;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> LobbyLevel;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> GameLevel;
+	
+private:
+	
+	void LoadLevelAndListen(TSoftObjectPtr<UWorld> Level);
+};
