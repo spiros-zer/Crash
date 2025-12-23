5905910c6db5810527f91437600f13fc389c545a
spiros <spiridonzervos@gmail.com>
2025-12-23 17:12:04 +0200

    235. Create the Character Display Class.

---
+++ Source/Crash/Private/Widgets/CharacterDisplay.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterDisplay.h"
+
+#include "Camera/CameraComponent.h"
+#include "Chatacter/PA_CharacterDefinition.h"
+
+
+ACharacterDisplay::ACharacterDisplay()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("RootComponent"));
+	
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>("MeshComponent");
+	MeshComponent->SetupAttachment(RootComponent);
+	
+	ViewCameraComponent = CreateDefaultSubobject<UCameraComponent>("ViewCameraComponent");
+	ViewCameraComponent->SetupAttachment(MeshComponent);
+}
+
+void ACharacterDisplay::ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* CharacterDefinition)
+{
+	check(CharacterDefinition);
+	
+	// Load meshes on-demand.
+	
+	MeshComponent->SetSkeletalMesh(CharacterDefinition->LoadDisplayMesh());
+	MeshComponent->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	MeshComponent->SetAnimInstanceClass(CharacterDefinition->LoadDisplayAnimBPClass());
+}
---
+++ Source/Crash/Private/Widgets/CharacterDisplay.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterDisplay.generated.h"
+
+class UPA_CharacterDefinition;
+class UCameraComponent;
+/** 
+ * Displays the character on the character selection screen.
+ */
+UCLASS()
+class CRASH_API ACharacterDisplay : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	
+	ACharacterDisplay();
+
+	void ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* CharacterDefinition);
+	
+private:
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	TObjectPtr<USkeletalMeshComponent> MeshComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	TObjectPtr<UCameraComponent> ViewCameraComponent;
+};
