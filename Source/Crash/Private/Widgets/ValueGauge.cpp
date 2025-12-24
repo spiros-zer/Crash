3e83a1ff1066ad23c39104d7fd14174773df2528
spiros <spiridonzervos@gmail.com>
2025-12-24 16:25:22 +0200

    23. Add the Value Gauge Wudget Class

---
+++ Source/Crash/Private/Widgets/ValueGauge.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ValueGauge.h"
+
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void UValueGauge::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	
+	ProgressBar->SetFillColorAndOpacity(BarColor);
+}
+
+void UValueGauge::SetValue(float NewValue, float NewMaxValue)
+{
+	// Setting progressbar
+	
+	if (!NewMaxValue)
+	{
+		SetVisibility(ESlateVisibility::Hidden);
+	
+		return;
+	}
+	
+	const float Percent = NewValue / NewMaxValue;
+	
+	ProgressBar->SetPercent(Percent);
+	
+	// Setting text
+	
+	FNumberFormattingOptions FormattingOptions;
+	FormattingOptions.SetMaximumFractionalDigits(0);
+	
+	FTextFormat TextFormat = FTextFormat::FromString("{0}/{1}");
+	FFormatNamedArguments Args;
+	Args.Add("{0}", FText::AsNumber(NewValue, &FormattingOptions));
+	Args.Add("{1}", FText::AsNumber(NewMaxValue, &FormattingOptions));
+	
+	ValueText->SetText(FText::Format(TextFormat, Args));
+}
---
+++ Source/Crash/Private/Widgets/ValueGauge.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ValueGauge.generated.h"
+
+class UTextBlock;
+class UProgressBar;
+/**
+ * 
+ */
+UCLASS()
+class CRASH_API UValueGauge : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void NativePreConstruct() override;
+	
+	void SetValue(float NewValue, float NewMaxValue);
+	
+private:
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> ValueText;
+	
+	UPROPERTY(EditAnywhere, Category = "Visual")
+	FLinearColor BarColor;
+};
