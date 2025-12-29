26f780182b2c3f3820b6069921d43bb4b1e90dfe
spiros <spiridonzervos@gmail.com>
2025-12-29 18:30:29 +0200

    55. Initialize Team ID and Start Spot for Players

--- Source/Crash/Private/Framework/CGameMode.cpp
+++ Source/Crash/Private/Framework/CGameMode.cpp
@@ -2,3 +2,53 @@
 
 
 #include "CGameMode.h"
+
+#include "EngineUtils.h"
+#include "GameFramework/PlayerStart.h"
+
+APlayerController* ACGameMode::SpawnPlayerController(ENetRole InRemoteRole, const FString& Options)
+{
+	APlayerController* PlayerController = Super::SpawnPlayerController(InRemoteRole, Options);
+	check(PlayerController);
+	
+	const FGenericTeamId TeamId = GetTeamIdForPlayer(PlayerController);
+
+	if (IGenericTeamAgentInterface* NewPlayerTeamInterface = Cast<IGenericTeamAgentInterface>(PlayerController))
+	{
+		NewPlayerTeamInterface->SetGenericTeamId(TeamId);
+	}	
+	
+	PlayerController->StartSpot = FindNextStartSpotForTeam(TeamId);
+	
+	return PlayerController;
+}
+
+FGenericTeamId ACGameMode::GetTeamIdForPlayer(const APlayerController* InPlayerController) const
+{
+	static int PlayerCount = 0;
+	++PlayerCount;
+	
+	return FGenericTeamId(PlayerCount % 2);
+}
+
+AActor* ACGameMode::FindNextStartSpotForTeam(const FGenericTeamId InTeamId)
+{
+	const FName* StartSpotTag = TeamStartSpotTagMap.Find(InTeamId);
+	if (!StartSpotTag) return nullptr;
+	
+	const UWorld* World = GetWorld();
+	check(World);
+
+	for (TActorIterator<APlayerStart> PlayerStart(World); PlayerStart; ++PlayerStart) 
+	{
+		if (PlayerStart->PlayerStartTag == *StartSpotTag)
+		{
+			// Mark as occupied to avoid players spawning on it
+			PlayerStart->PlayerStartTag = FName("Taken");
+			
+			return *PlayerStart;
+		}
+	}
+	
+	return nullptr;
+}
--- Source/Crash/Private/Framework/CGameMode.h
+++ Source/Crash/Private/Framework/CGameMode.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
 #include "GameFramework/GameModeBase.h"
 #include "CGameMode.generated.h"
 
@@ -10,7 +11,28 @@
  * 
  */
 UCLASS()
-class CRASH_API ACGameMode : public AGameModeBase
+class CRASH_API ACGameMode : public AGameModeBase, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
+	
+public:
+	
+	/** 
+	 * Called when the player is to be spawned (locally or multiplayer) and before the pawn is spawned.
+	 */
+	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, const FString& Options) override;
+	
+protected:
+	
+	/** 
+	 * Holds the team id to a player start tag.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "Team")
+	TMap<FGenericTeamId, FName> TeamStartSpotTagMap;
+	
+private:
+	
+	FGenericTeamId GetTeamIdForPlayer(const APlayerController* InPlayerController) const;
+	
+	AActor* FindNextStartSpotForTeam(const FGenericTeamId InTeamId);
 };
