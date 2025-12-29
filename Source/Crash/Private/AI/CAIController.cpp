d0df36c582858d5188da7280c70bb9b99abf05e4
spiros <spiridonzervos@gmail.com>
2025-12-30 00:05:14 +0200

    58. Add AI and Configure the AI TeamID and Sight Perception

---
+++ Source/Crash/Private/AI/CAIController.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CAIController.h"
+
+#include "Chatacter/CCharacter.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+ACAIController::ACAIController()
+{
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("SightConfig");
+	check(IsValid(SightConfig));
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
+	SightConfig->SightRadius = SightRadius;
+	SightConfig->LoseSightRadius = LoseSightRadius;
+	SightConfig->SetMaxAge(SightMaxAge);
+	SightConfig->PeripheralVisionAngleDegrees = 180.f;
+	
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AIPerceptionComponent");
+	check(IsValid(AIPerceptionComponent));
+	AIPerceptionComponent->ConfigureSense(*SightConfig);
+}
+
+void ACAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	SetGenericTeamId(FGenericTeamId(0));
+
+	if (IGenericTeamAgentInterface* PawnTeamInterface = Cast<IGenericTeamAgentInterface>(InPawn))
+	{
+		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
+	}
+}
---
+++ Source/Crash/Private/AI/CAIController.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CAIController.generated.h"
+
+class UAISenseConfig_Sight;
+
+UCLASS()
+class CRASH_API ACAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+
+	ACAIController();
+	
+	virtual void OnPossess(APawn* InPawn) override;
+	
+protected:
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Perception")
+	float SightRadius = 1000.f;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	float LoseSightRadius = 1200.f;
+	
+	/** 
+	 * Time until the AI forgets you when leaving its sight.
+	 */
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	float SightMaxAge = 5.f;
+};
